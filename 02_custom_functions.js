e7dc23b3ab13c9f7e12028215d5f3f5aff56aa3f
frankatimm <83537293+frankatimm@users.noreply.github.com>
2022-01-21 16:34:11 +0100

    Add files via upload

--- 02_custom_functions.js
+++ 02_custom_functions.js
@@ -31,7 +31,7 @@ const generateID = function(len) {
 
 
 
-/* Hooks  
+/* Hooks
 *
 *
 */
@@ -68,3 +68,35 @@ check_response = function(data, next) {
 *
 *
 */
+
+const custom_views = {};
+
+custom_views.multi_choice_customized = function(config) {
+      const multi_choice_customized_function = magpieViews.view_generator(
+          "multi_choice",
+          // config information
+          {
+              trials: forced_choice_3A_trials.length,
+              name: 'Forced Choice 3A',
+              data: forced_choice_3A_trials
+          },
+          // custom generator functions
+          {
+            answer_container_generator: function (config, CT) {
+             return `<div class='magpie-view-answer-container'>
+                     <p class='magpie-view-question'>${config.data[CT].question}</p>
+                     <label for='o1' class='magpie-response-buttons'>${config.data[CT].option1}</label>
+                     <input type='radio' name='answer' id='o1' value=${config.data[CT].option1} />
+                     <label for='o2' class='magpie-response-buttons'>${config.data[CT].option2}</label>
+                     <input type='radio' name='answer' id='o2' value=${config.data[CT].option2} />
+                     <label for='o2' class='magpie-response-buttons'>${config.data[CT].option3}</label>
+                     <input type='radio' name='answer' id='o3' value=${config.data[CT].option3} />
+                     </div>`;
+        }
+          }
+
+      );
+      return multi_choice_customized_function;
+
+
+};
