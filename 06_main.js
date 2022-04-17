f7eb32142f5d8a551e2dd5bad13faa50279d3d59
frankatimm <83537293+frankatimm@users.noreply.github.com>
2022-04-17 17:53:28 +0200

    Add files via upload

---
+++ 06_main.js
@@ -0,0 +1,61 @@
+// In this file you initialize and configure your experiment using magpieInit
+
+$("document").ready(function() {
+    // prevent scrolling when space is pressed
+    window.onkeydown = function(e) {
+        if (e.keyCode === 32 && e.target === document.body) {
+            e.preventDefault();
+        }
+    };
+
+    // calls magpieInit
+    // in debug mode this returns the magpie-object, which you can access in the console of your browser
+    // e.g. >> window.magpie_monitor or window.magpie_monitor.findNextView()
+    // in all other modes null will be returned
+    window.magpie_monitor = magpieInit({
+        // Here, we specify all views you want to use in this experiment and the order of them
+        views_seq: [
+          // introduction
+          intro,
+          // Phase 1 --- filler sentences
+          textbox_input_instructions,
+          filler_sentences,
+          // Phase 2 --- name review
+          name_check_instructions,
+          name_check,
+          // Phase 3 --- stereotype check
+          stereotype_check_instructions,
+          stereotype_check,
+          //post test and thanks
+          post_test_customized,
+          thanks,
+        ],
+        // Here, we specify all information for the deployment
+        deploy: {
+            experimentID: "302",
+            serverAppURL: "https://magpie-demo.herokuapp.com/api/submit_experiment/",
+            // Possible deployment methods are:
+            // "debug" and "directLink"
+            // As well as "MTurk", "MTurkSandbox" and "Prolific"
+            deployMethod: "debug",
+            contact_email: "franka.timm00@gmail.com",
+            prolificURL: "https://app.prolific.ac/submissions/complete?cc=SAMPLE1234"
+        },
+        // Here, we specify how the progress bar should look like
+        progress_bar: {
+            in: [
+              // list the view-names of the views for which you want a progress bar
+              textbox_input_instructions.name,
+              filler_sentences.name,
+              name_check_instructions.name,
+              name_check.name,
+              stereotype_check_instructions.name,
+              stereotype_check.name,
+              post_test_customized.name,
+            ],
+             // Possible styles are "default", "separate" and "chunks"
+            style: "separate",
+            width: 100
+        }
+    });
+});
