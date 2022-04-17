fd31bb08c467d2607878e8419142cc7f99c13f63
frankatimm <83537293+frankatimm@users.noreply.github.com>
2022-04-17 19:44:39 +0200

    add comments

--- Soft Cosine Measure/Generic_masculine/most_frequent_generic_masculine_rolenouns.py
+++ Soft Cosine Measure/Generic_masculine/most_frequent_generic_masculine_rolenouns.py
@@ -1,49 +1,51 @@
 r"""
-Most frequent nouns in Corpus with generic masculine role nouns
+Most frequent generic masculine nouns
 ==================
 
-Demonstrates filtering the original corpus in order to find key words for later Soft Cosine Measure
+Demonstrates searching through the generic-masculine corpus in order to find key words for later Soft Cosine Measure.
+The key words are generic masculine nouns that share the ending "er".
 """
 
 
-#import packages
+# import packages
 import regex as re
 from collections import Counter
 import codecs
 
-#open text file in read mode
-text_file = open('C:/Users/Franka/Wahlprogrammetext/full_right.txt', "r", encoding="utf8")
+# open text file in read mode
+text_file = open('C:/Users/Franka/Soft_Cosine_Measure/Generic_masculine/corpus_generic_masculine.txt', "r", encoding="utf8")
 
-#read whole file to a string
+# read whole file to a string
 data = text_file.read()
 
-#close file
+# close file
 text_file.close()
 
-#filter words with captial initial
+# filter words with captial initial and returns them as a string
 def filterupper(text):
     return " ".join([word for word in text.split() if word[0].isupper()])
 
+# save as uppercase
 uppercase = filterupper(data)
 
-#define stop_words
-stop_words = 'Ab Aber Ach Acht Achte Achten Achter Achtes Ag Alle Allein Allem Allen Aller Allerdings Alles Allgemeinen Als Also Am An Andere Anderen Andern Anders Au Auch Auf Aus Ausser Außer Ausserdem Außerdem Bald Bei Beide Beiden Beim Beispiel Bekannt Bereits Besonders Besser Besten Bin Bis Bisher Bist Da Dabei Dadurch Dafür Dagegen Daher Dahin Dahinter Damals Damit Danach Daneben Dank Dann Daran Darauf Daraus Darf Darfst Darin Darüber Darum Darunter Das Dasein Daselbst Dass Daß Dasselbe Davon Davor Dazu Dazwischen Dein Deine Deinem Deiner Dem Dementsprechend Demgegenüber Demgemäss Demgemäß Demselben Demzufolge Den Denen Denn Denselben Der Deren Derjenige Derjenigen Dermassen Dermaßen Derselbe Derselben Des Deshalb Desselben Dessen Deswegen D.h Dich Die Diejenige Diejenigen Dies Diese Dieselbe Dieselben Diesem Diesen Dieser Dieses Dir Doch Dort Drei Drin Dritte Dritten Dritter Drittes Du Durch Durchaus Dürfen Dürft Durfte Durften Eben Ebenso Ehrlich Ei Ei, Eigen Eigene Eigenen Eigener Eigenes Ein Einander Eine Einem Einen Einer Eines Einige Einigen Einiger Einiges Einmal Eins Elf En Ende Endlich Entweder Er Ernst Erst Erste Ersten Erster Erstes Es Etwa Etwas Euch Früher Fünf Fünfte Fünften Fünfter Fünftes Für Gab Ganz Ganze Ganzen Ganzer Ganzes Gar Gedurft Gegen Gegenüber Gehabt Gehen Geht Gekannt Gekonnt Gemacht Gemocht Gemusst Genug Gerade Gern Gesagt Geschweige Gewesen Gewollt Geworden Gibt Ging Gleich Gott Gross Groß Grosse Große Grossen Großen Grosser Großer Grosses Großes Gut Gute Guter Gutes Habe Haben Habt Hast Hat Hatte Hätte Hatten Hätten Heisst Her Heute Hier Hin Hinter Hoch Ich Ihm Ihn Ihnen Ihr Ihre Ihrem Ihren Ihrer Ihres Im Immer In Indem Infolgedessen Ins Irgend Ist Ja Jahr Jahre Jahren Je Jede Jedem Jeden Jeder Jedermann Jedermanns Jedoch Jemand Jemandem Jemanden Jene Jenem Jenen Jener Jenes Jetzt Kam Kann Kannst Kaum Kein Keine Keinem Keinen Keiner Kleine Kleinen Kleiner Kleines Kommen Kommt Können Könnt Konnte Könnte Konnten Kurz Lang Lange Leicht Leide Lieber Los Machen Macht Machte Mag Magst Mahn Man Manche Manchem Manchen Mancher Manches Mann Mehr Mein Meine Meinem Meinen Meiner Meines Mensch Menschen Mich Mir Mit Mittel Mochte Möchte Mochten Mögen Möglich Mögt Morgen Muss Muß Müssen Musst Müsst Musste Mussten Na Nach Nachdem Nahm Natürlich Neben Nein Neue Neuen Neun Neunte Neunten Neunter Neuntes Nicht Nichts Nie Niemand Niemandem Niemanden Noch Nun Nur Ob Oben Oder Offen Oft Ohne Ordnung Recht Rechte Rechten Rechter Rechtes Richtig Rund Sa Sache Sagt Sagte Sah Satt Schlecht Schluss Schon Sechs Sechste Sechsten Sechster Sechstes Sehr Sei Seid Seien Sein Seine Seinem Seinen Seiner Seines Seit Seitdem Selbst Sich Sie Sieben Siebente Siebenten Siebenter Siebentes Sind So Solang Solche Solchem Solchen Solcher Solches Soll Sollen Sollte Sollten Sondern Sonst Sowie Später Statt Tag Tage Tagen Tat Teil Tel Tritt Trotzdem Tun Über Überhaupt Übrigens Uhr Um Und Und? Uns Unser Unsere Unserer Unter Vergangenen Viel Viele Vielem Vielen Vielleicht Vier Vierte Vierten Vierter Viertes Vom Von Vor Wahr? Während Währenddem Währenddessen Wann War Wäre Waren Wart Warum Was Wegen Weil Weit Weiter Weitere Weiteren Weiteres Welche Welchem Welchen Welcher Welches Wem Wen Wenig Wenige Weniger Weniges Wenigstens Wenn Wer Werde Werden Werdet Wessen Wie Wieder Will Willst Wir Wird Wirklich Wirst Wo Wohl Wollen Wollt Wollte Wollten Worden Wurde Würde Wurden Würden Z.b Zehn Zehnte Zehnten Zehnter Zehntes Zeit Zu Zuerst Zugleich Zum Zunächst Zur Zurück Zusammen Zwanzig Zwar Zwei Zweite Zweiten Zweiter Zweites Zwischen Zwölf Euer Eure Hattest Hattet Jedes Mußt Müßt Sollst Sollt Soweit Weshalb Wieso Woher Wohin ab aber ach acht achte achten achter achtes ag alle allein allem allen aller allerdings alles allgemeinen als also am an andere anderen andern anders au auch auf aus ausser außer ausserdem außerdem bald bei beide beiden beim beispiel bekannt bereits besonders besser besten bin bis bisher bist da dabei dadurch dafür dagegen daher dahin dahinter damals damit danach daneben dank dann daran darauf daraus darf darfst darin darüber darum darunter das dasein daselbst dass daß dasselbe davon davor dazu dazwischen dein deine deinem deiner dem dementsprechend demgegenüber demgemäss demgemäß demselben demzufolge den denen denn denselben der deren derjenige derjenigen dermassen dermaßen derselbe derselben des deshalb desselben dessen deswegen d.h dich die diejenige diejenigen dies diese dieselbe dieselben diesem diesen dieser dieses dir doch dort drei drin dritte dritten dritter drittes du durch durchaus dürfen dürft durfte durften eben ebenso ehrlich ei ei, eigen eigene eigenen eigener eigenes ein einander eine einem einen einer eines einige einigen einiger einiges einmal eins elf en ende endlich entweder er ernst erst erste ersten erster erstes es etwa etwas euch früher fünf fünfte fünften fünfter fünftes für gab ganz ganze ganzen ganzer ganzes gar gedurft gegen gegenüber gehabt gehen geht gekannt gekonnt gemacht gemocht gemusst genug gerade gern gesagt geschweige gewesen gewollt geworden gibt ging gleich gott gross groß grosse große grossen großen grosser großer grosses großes gut gute guter gutes habe haben habt hast hat hatte hätte hatten hätten heisst her heute hier hin hinter hoch ich ihm ihn ihnen ihr ihre ihrem ihren ihrer ihres im immer in indem infolgedessen ins irgend ist ja jahr jahre jahren je jede jedem jeden jeder jedermann jedermanns jedoch jemand jemandem jemanden jene jenem jenen jener jenes jetzt kam kann kannst kaum kein keine keinem keinen keiner kleine kleinen kleiner kleines kommen kommt können könnt konnte könnte konnten kurz lang lange leicht leide lieber los machen macht machte mag magst mahn man manche manchem manchen mancher manches mann mehr mein meine meinem meinen meiner meines mensch menschen mich mir mit mittel mochte möchte mochten mögen möglich mögt morgen muss muß müssen musst müsst musste mussten na nach nachdem nahm natürlich neben nein neue neuen neun neunte neunten neunter neuntes nicht nichts nie niemand niemandem niemanden noch nun nur ob oben oder offen oft ohne ordnung recht rechte rechten rechter rechtes richtig rund sa sache sagt sagte sah satt schlecht schluss schon sechs sechste sechsten sechster sechstes sehr sei seid seien sein seine seinem seinen seiner seines seit seitdem selbst sich sie sieben siebente siebenten siebenter siebentes sind so solang solche solchem solchen solcher solches soll sollen sollte sollten sondern sonst sowie später statt tag tage tagen tat teil tel tritt trotzdem tun über überhaupt übrigens uhr um und und? uns unser unsere unserer unter vergangenen viel viele vielem vielen vielleicht vier vierte vierten vierter viertes vom von vor wahr? während währenddem währenddessen wann war wäre waren wart warum was wegen weil weit weiter weitere weiteren weiteres welche welchem welchen welcher welches wem wen wenig wenige weniger weniges wenigstens wenn wer werde werden werdet wessen wie wieder will willst wir wird wirklich wirst wo wohl wollen wollt wollte wollten worden wurde würde wurden würden z.b zehn zehnte zehnten zehnter zehntes zeit zu zuerst zugleich zum zunächst zur zurück zusammen zwanzig zwar zwei zweite zweiten zweiter zweites zwischen zwölf euer eure hattest hattet jedes mußt müßt sollst sollt soweit weshalb wieso woher wohin'
+# define stop_words (captial initial)
+stop_words = 'Ab Aber Ach Acht Achte Achten Achter Achtes Ag Alle Allein Allem Allen Aller Allerdings Alles Allgemeinen Als Also Am An Andere Anderen Andern Anders Au Auch Auf Aus Ausser Außer Ausserdem Außerdem Bald Bei Beide Beiden Beim Beispiel Bekannt Bereits Besonders Besser Besten Bin Bis Bisher Bist Da Dabei Dadurch Dafür Dagegen Daher Dahin Dahinter Damals Damit Danach Daneben Dank Dann Daran Darauf Daraus Darf Darfst Darin Darüber Darum Darunter Das Dasein Daselbst Dass Daß Dasselbe Davon Davor Dazu Dazwischen Dein Deine Deinem Deiner Dem Dementsprechend Demgegenüber Demgemäss Demgemäß Demselben Demzufolge Den Denen Denn Denselben Der Deren Derjenige Derjenigen Dermassen Dermaßen Derselbe Derselben Des Deshalb Desselben Dessen Deswegen D.h Dich Die Diejenige Diejenigen Dies Diese Dieselbe Dieselben Diesem Diesen Dieser Dieses Dir Doch Dort Drei Drin Dritte Dritten Dritter Drittes Du Durch Durchaus Dürfen Dürft Durfte Durften Eben Ebenso Ehrlich Ei Ei, Eigen Eigene Eigenen Eigener Eigenes Ein Einander Eine Einem Einen Einer Eines Einige Einigen Einiger Einiges Einmal Eins Elf En Ende Endlich Entweder Er Ernst Erst Erste Ersten Erster Erstes Es Etwa Etwas Euch Früher Fünf Fünfte Fünften Fünfter Fünftes Für Gab Ganz Ganze Ganzen Ganzer Ganzes Gar Gedurft Gegen Gegenüber Gehabt Gehen Geht Gekannt Gekonnt Gemacht Gemocht Gemusst Genug Gerade Gern Gesagt Geschweige Gewesen Gewollt Geworden Gibt Ging Gleich Gott Gross Groß Grosse Große Grossen Großen Grosser Großer Grosses Großes Gut Gute Guter Gutes Habe Haben Habt Hast Hat Hatte Hätte Hatten Hätten Heisst Her Heute Hier Hin Hinter Hoch Ich Ihm Ihn Ihnen Ihr Ihre Ihrem Ihren Ihrer Ihres Im Immer In Indem Infolgedessen Ins Irgend Ist Ja Jahr Jahre Jahren Je Jede Jedem Jeden Jeder Jedermann Jedermanns Jedoch Jemand Jemandem Jemanden Jene Jenem Jenen Jener Jenes Jetzt Kam Kann Kannst Kaum Kein Keine Keinem Keinen Keiner Kleine Kleinen Kleiner Kleines Kommen Kommt Können Könnt Konnte Könnte Konnten Kurz Lang Lange Leicht Leide Lieber Los Machen Macht Machte Mag Magst Mahn Man Manche Manchem Manchen Mancher Manches Mann Mehr Mein Meine Meinem Meinen Meiner Meines Mensch Menschen Mich Mir Mit Mittel Mochte Möchte Mochten Mögen Möglich Mögt Morgen Muss Muß Müssen Musst Müsst Musste Mussten Na Nach Nachdem Nahm Natürlich Neben Nein Neue Neuen Neun Neunte Neunten Neunter Neuntes Nicht Nichts Nie Niemand Niemandem Niemanden Noch Nun Nur Ob Oben Oder Offen Oft Ohne Ordnung Recht Rechte Rechten Rechter Rechtes Richtig Rund Sa Sache Sagt Sagte Sah Satt Schlecht Schluss Schon Sechs Sechste Sechsten Sechster Sechstes Sehr Sei Seid Seien Sein Seine Seinem Seinen Seiner Seines Seit Seitdem Selbst Sich Sie Sieben Siebente Siebenten Siebenter Siebentes Sind So Solang Solche Solchem Solchen Solcher Solches Soll Sollen Sollte Sollten Sondern Sonst Sowie Später Statt Tag Tage Tagen Tat Teil Tel Tritt Trotzdem Tun Über Überhaupt Übrigens Uhr Um Und Und? Uns Unser Unsere Unserer Unter Vergangenen Viel Viele Vielem Vielen Vielleicht Vier Vierte Vierten Vierter Viertes Vom Von Vor Wahr? Während Währenddem Währenddessen Wann War Wäre Waren Wart Warum Was Wegen Weil Weit Weiter Weitere Weiteren Weiteres Welche Welchem Welchen Welcher Welches Wem Wen Wenig Wenige Weniger Weniges Wenigstens Wenn Wer Werde Werden Werdet Wessen Wie Wieder Will Willst Wir Wird Wirklich Wirst Wo Wohl Wollen Wollt Wollte Wollten Worden Wurde Würde Wurden Würden Z.b Zehn Zehnte Zehnten Zehnter Zehntes Zeit Zu Zuerst Zugleich Zum Zunächst Zur Zurück Zusammen Zwanzig Zwar Zwei Zweite Zweiten Zweiter Zweites Zwischen Zwölf Euer Eure Hattest Hattet Jedes Mußt Müßt Sollst Sollt Soweit Weshalb Wieso Woher Wohin'
 
-#split stopwords
-#return list of all stopwords
+# split stop_words and return list of all stopwords
 stop_words = stop_words.split()
 
-#remove stopwords from all uppercase words
+# function removes stop_words from all uppercase words and returns them as a string
 def removestopwords(text):
     return " ".join([word for word in text.split() if word not in stop_words])
 
+# save as words
 words = removestopwords(uppercase)
 
-#filter all words that end with generic form "er" from words
+# function filters all words that end with generic form "er" from words and returns them as a string
 def ending_with(ending, text):
     return " ".join([word for word in text.split() if word.endswith(ending)])
 
-#safe as generics
+# save as generics
 generics = ending_with('er', words)
 
 
@@ -59,5 +61,5 @@ most_occur = Counter.most_common(500)
 
 print(most_occur)
 
-# von konsole speichern:
-# python mein_programm.py > most_frequent_generic-masculine-nouns.txt
+# save output from the console in text file:
+# python most_frequent_generic_masculine_rolenouns.py > most_frequent_generic-masculine-nouns.txt
--- Soft Cosine Measure/Generic_masculine/soft_cosine_measure_generic_masculine.py
+++ Soft Cosine Measure/Generic_masculine/soft_cosine_measure_generic_masculine.py
@@ -1,97 +1,98 @@
-#https://radimrehurek.com/gensim/auto_examples/tutorials/run_word2vec.html#introducing-the-word2vec-model
+r"""
+Computing the Soft Cosine Measure
+===================
+
+Demonstrates using Gensim's implemenation of the SCM.
+Compares basic query with generic-masculine query.
+"""
+
 # Initialize logging.
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
+# import packages
 from gensim.test.utils import datapath
 from gensim import utils
 
+# training our own model on our gender-neutral corpus
+# implement a memory-friendly iterator that reads it line-by-line
 class MyCorpus:
     """An iterator that yields sentences (lists of str)."""
 
     def __iter__(self):
-        #corpus_path = datapath('C:/Users/Franka/Wahlprogrammetext/leftwing.txt')
         for line in open('C:/Users/Franka/Soft_Cosine_Measure/Generic_masculine/corpus_generic_masculine.txt',encoding="utf8"):
             # assume there's one document per line, tokens separated by whitespace
             yield utils.simple_preprocess(line)
 
+# import packages
 import gensim.models
 
+# train model on generic_masculine corpus
 sentences = MyCorpus()
 model = gensim.models.Word2Vec(sentences=sentences)
 
 
-#retrieving vocabulary
+# retrieve the vocabulary of our model
 for index, word in enumerate(model.wv.index_to_key):
     if index == 10:
         break
     print(f"word #{index}/{len(model.wv.index_to_key)} is {word}")
 
-# import tempfile
-#
-# with tempfile.NamedTemporaryFile(prefix='gensim-model-', delete=False) as tmp:
-#     temporary_filepath = tmp.name
-#     model.save(temporary_filepath)
-#     #
-#     # The model is now safely stored in the filepath.
-#     # You can copy it to other machines, share it with others, etc.
-#     #
-#     # To load a saved model:
-#     #
-#     new_model = gensim.models.Word2Vec.load(temporary_filepath)
-#
-# model = gensim.models.KeyedVectors.load_word2vec_format('C:/Users/Franka/Wahlprogrammetext/', binary=False)
 
-# #Initialize queries for comparison
-# query_basic = 'Mann und Männer und Frau und Frauen'
-# query_wähler_gmasc = 'Wähler'
-# query_wähler_gneu = 'Wählerin und Wählerinnen und Wähler'
-#
+# Initialize queries
 
-
-
-# query_basic =  'Mann, Männer, Frau, Frauen, Kinder, Menschen, Mensch, Jugendliche, Teenager, Familie, Jung, Alt, Identität, Religion, Kultur, Herkunft, Gesellschaft, Deutschland, Ehefrau, Ehemann, Ehepaar'
+# basic query with most frequent nouns
 query_basic = 'Mann, Männer, Frau, Frauen, Kinder, Menschen, Mensch, Jugendliche, Teenager, Familie, Jung, Alt, Identität, Religion, Kultur, Herkunft, Gesellschaft, Deutschland, Ehefrau, Ehemann, Ehepaar Mensch Deutschland  Land  Mensch  Demokrat Europa Staat Afd Kind  Unternehmen  Bildung Chance  Jahr  Bürger  Familie  Zukunft  Eu  Wirtschaft  Sicherheit  Freiheit  Ziel  Schule  Euro  Entwicklung  Welt  Gesellschaft  Weg  Schutz  Möglichkeit  Leistung  Arbeit  Seite  Verantwortung  Politik  Leben  Recht  Zeit  Union  Bereich  Digitalisierung  Forschung  Demokratie  Arbeitsplatz  Beispiel  Wettbewerb  Rahmen  Wohlstand  Ausbau  Bund  Interesse  Förderung  Maßnahme  Prozent  Frau  Beitrag  Zusammenarbeit  Aufgabe  Ausbildung  Investition  Infrastruktur  Mittel  Wachstum  Teil  Steuer  Hochschule  Geld  Einführung  Integration  Versorgung  Ebene  Herausforderung  Entscheidung  Grundlage  Eltern  Raum  Regel  Marktwirtschaft  Landwirtschaft  Bürgerinnen  Datum  Partner  Kultur  Rahmenbedingung  Dienst  Energie  Region  Chen  Stärkung  Einsatz  Voraussetzung  Gesetz  Betrieb  Ge  Programm  Finanzierung  Kommune  Innovation  Vielfalt  Rente  Lösung  Hilfe  Cdu  Csu  Technologie  Unterstützung  Reform  Mittelstand  Prinzip  Erfolg  Beruf  Altersvorsorge  Schritt  Bürokratie  Zugang  Form  Idee  Generation  Bundestagswahl  Rechtsstaat  Partei  Regelung  Bundesregierung  Markt  Pflege  Mobilität  Grundgesetz  Ort  Schüler  Nutzung  Million  Arbeitsmarkt  Angebot  Anreiz  Einkommen  Alter  Arbeitnehmer  Grenze  Abschaffung  Kosten  Bundeswehr  Blick  Menschenrecht  Stadt  Zahl  Verwaltung  Wert  Frieden  Migration  Nation  Volk  Milliarde  Rolle  Parlament  Art  Standard  Einrichtung  Kraft  Nato  Kontorolle  Sprache  Bedeutung  Arbeitgeber  Produkt  Ezb  Fortschritt  Wettbewerbsfähigkeit  Verbraucher  Asyl  Selbstbestimmung  Industrie  Höhe  Anspruch  Internet  %  Studierend  Schuld  Verbesserung  Instrument  Sinn  Hand  Ausland  Grund  Wissenschaft  Mann  Beschäftigt  Behinderung  Polizei  Kooperation  Fall  System  Usa  Lernen  Stärke  Problem  Regierungsprogramm  Türkei  Vorbild Patient Finanz Kriminalität Währung'
 
+# generic_masculine query with most frequent masculine generic nouns
 query_generic_masculine = '  Kinder  Länder   Lehrer  Dauer  Schweizer  Steuerzahler  Ferner  Mitarbeiter  Zeitalter  Mitglieder  Männer  Träger  Opfer  Wälder  Asylbewerber  Krankenhäuser  Erbschaftsteuer  Gründer  Täter  Rentner  Miteinander  Digitalsteuer  Mutter  Väter  Völker  Bundesländer  Vermögensteuer  Unternehmer  Anbieter  Grunderwerbsteuer  Umsatzsteuer  Gewerbesteuer  Einkommensteuer  Wasser  Tarifpartner  Sparer  Pariser  Regierungspolitiker  Vorreiter  Minister  Stabilitätsanker  Verkehrsträger  Hinweisgeber  Vaterländer  Mieter  Impulsgeber  Wähler  Vermögenssteuer  Moderner  Gelder  Forscher  Sozialer  Straftäter  Neuer  Politiker  Master  Sozialgesetzbücher  Beitragszahler  Stromsteuer  Fächer  Krimineller  Rentenalter  Fairer  Digitalzeitalter  Transfer  Güter  Richter  Meister  Freier  Mütter  Magister  Internationaler  Urheber  Leistungsträger  Gefährder  Partnerländer  Bewerber  Ausländer  Einwanderer  Geschlechter  Entwicklungsländer  Bezugsdauer  Leider  September  Vorbilder Handwerker  Vater  Digitaler  Bundeskanzler  Nutzer  Künstlicher  Riester  Nummer  Vertreter  Wirtschaftsgüter  Genfer  Geldgeber  Ämter  Leiharbeitnehmer  Parlamentarier  Gläubiger  Grundpfeiler  Mittelmeer  Volksvertreter  Finger  Leistungsempfänger  Bündnispartner  Hersteller  Grundsteuer  Wissenschaftler  Hunger  Mandatsträger  Besserer  Zentraler  Europäer  Flexiblerer  Aussiedler  Aktivierender  Whistleblower  Fehler  Erzieher  Biersteuer  Flexibler  Newcomer  Wettbewerber  Erneuerbarer  Nebeneinander  Neugier  Klassenzimmer  Auftraggeber  Pfeiler  Herkunftsländer  Renteneintrittsalter  Betreiber  Anleger  CO-neutraler  Zweitwohnungssteuer  MINT-Fächer  Zuwanderer  Deutscher  Finanztransaktionssteuer  Jugendämter  Geber  Angehöriger  Energieträger  Königsteiner  Justizminister  Gegner  Effektiver  Sozialpartner  Oktober  Eigentümer  Berliner  Jäger  Sicher  Landesämter  Entscheidungsträger  Freiberufler  Strafmündigkeitsalter  November  EU-Ausländer  Mehrwertsteuer  Technischer  Eckpfeiler  Schaumweinsteuer  Fischereisteuer  Generationsübergreifender  Treiber  Kindesalter  Nachhaltiger  Bezieher  Verfassungsrechtler  Einwohner  Körperschaftsteuer  Einkommensteuerzahler  Speicher  Voller  Erfinder  Quereinsteiger  Mehrgenerationenhäuser  Wirksamer  Beschäftigtenvertreter  Vermieter  Rentenbezieher  Gewässer  Zeitarbeitnehmer  Per  Lindner  Finanzminister  Schiller  Regenwälder  Weimarer  Semester  Anwohner  Gesetzgeber  Wachstumstreiber  Handelspartner  Entwickler  Gesellschafter  Weder  Trinkwasser  Erzeuger  Kleinkinder  Stromzähler  Wider  Schlecker  Ungeborener  Ehepartner  Luftverkehrssteuer  Schuldner  Fördergelder  Existenzgründer  Steuerschlupflöcher  Tier  Marschflugkörper  Legalwaffenbesitzer  Wiener  Gemeinsamer  Stimmungsbilder  Schulabgänger  Clan-Mitglieder  Flüchtlingskinder  Aufenthaltsdauer  Marktteilnehmer  Sportler  Personenstandsregister  Standesämter  Abgeltungsteuer  Berufsschüler  Fürsprecher  Prozentualer  Automobilhersteller  Deutsch-französischer  Aachener  Spätaussiedler  Kandidatenländer  Urlaubssemester  EU-Bürger  Berufsfelder  Niedriglohnempfänger  Abgelehnter  Asylantragsteller  Schwellenländer  Verfahrensdauer  EU-Länder  Endlager  Tech-Gründer  Ausreisepflichtiger  Erbschaftssteuer  Dienstleister  Identitätstäuscher  Mitwirkungsverweigerer  Datenträger  Mitbürger  Papier  Alzheimer  Amtsträger  Quantencomputer  Provider  Parlamentarischer'
 
-
 #similarity = 0.0816 für "query_basic" und "query_generic_masculine"
+
+
 # Import and download stopwords from NLTK.
 from nltk.corpus import stopwords
 from nltk import download
 download('stopwords')  # Download stopwords list.
 stop_words = stopwords.words('german')
 
+# Before we compute the SCM, we do some preprocessing
+# we want to remove stopwords as these do not contribute a lot to the information in the sentences
+# split sentences into words and
+# lowercasing each word
 def preprocess(sentence):
     return [w for w in sentence.lower().split() if w not in stop_words]
 
+# preprocess queries
 query_basic = preprocess(query_basic)
 query_generic_masculine = preprocess(query_generic_masculine)
 
-print(query_basic, query_generic_masculine)
 
+# import packages
 from gensim.corpora import Dictionary
+
+# build a dictionary and a TF-IDF model, and convert the sentences to the bag-of-words format
 documents = [query_basic, query_generic_masculine]
 dictionary = Dictionary(documents)
 
 query_basic = dictionary.doc2bow(query_basic)
-
 query_generic_masculine = dictionary.doc2bow(query_generic_masculine)
 
-
+# import packages
 from gensim.models import TfidfModel
+
 documents = [query_basic, query_generic_masculine]
 tfidf = TfidfModel(documents)
 
 query_basic = tfidf[query_basic]
-
 query_generic_masculine = tfidf[query_generic_masculine]
 
-
+# import packages
 from gensim.similarities import SparseTermSimilarityMatrix, WordEmbeddingSimilarityIndex
-termsim_index = WordEmbeddingSimilarityIndex(model.wv)
 
+# load model into a Gensim Word2Vec model class
+# and build a term similarity mextrix using the embeddings
+termsim_index = WordEmbeddingSimilarityIndex(model.wv)
 termsim_matrix = SparseTermSimilarityMatrix(termsim_index, dictionary, tfidf)
 
 
